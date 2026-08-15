6d98a6e00336ecf9d613a13d844674c073496e13
Jasper Boner <jboner30@brophybroncos.org>
2026-08-15 15:12:37 -0700

    Slighty randomized enemy speed

--- game.js
+++ game.js
@@ -108,7 +108,10 @@ function spawnEnemy() {
     y = Math.random() * height;
   }
 
-  const speed = 85 + Math.min(score * 2, 180);
+  const baseSpeed = 85 + Math.min(score * 2, 180);
+  const speedVariation = Math.random() * 20 - 10; // -10 through +10
+  const speed = baseSpeed + speedVariation;
+
   const direction = Math.atan2(centerY - y, centerX - x);
 
   enemies.push({
